41b64592d520060be0d0d734b7bfb748b63ea0df
Jurfest <jurfest.apps@gmail.com>
2025-09-21 16:42:39 -0300

    chore: organize folders

--- src/app/validators/custom-validators.ts
+++ src/app/validators/custom-validators.ts
@@ -4,22 +4,22 @@ export class CustomValidators {
   static cpf(): ValidatorFn {
     return (control: AbstractControl): ValidationErrors | null => {
       if (!control.value) return null;
-      
+
       const cpf = control.value.replace(/\D/g, '');
       if (cpf.length !== 11 || /^(\d)\1{10}$/.test(cpf)) return { cpf: true };
-      
+
       let sum = 0;
       for (let i = 0; i < 9; i++) sum += parseInt(cpf[i]) * (10 - i);
       let digit = 11 - (sum % 11);
       if (digit > 9) digit = 0;
       if (parseInt(cpf[9]) !== digit) return { cpf: true };
-      
+
       sum = 0;
       for (let i = 0; i < 10; i++) sum += parseInt(cpf[i]) * (11 - i);
       digit = 11 - (sum % 11);
       if (digit > 9) digit = 0;
       if (parseInt(cpf[10]) !== digit) return { cpf: true };
-      
+
       return null;
     };
   }

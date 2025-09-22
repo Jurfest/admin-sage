37293451a0ca7f564bc1c6ff682eb349f2b66590
Jurfest <jurfest.apps@gmail.com>
2025-09-22 00:50:30 -0300

    style: format files

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,9 @@
-import { Component, ChangeDetectionStrategy, inject, computed } from '@angular/core';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  inject,
+  computed,
+} from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SpinnerComponent } from './ui/spinner/spinner.component';
 import { LoadingStore } from './shared/stores/loading.store';
@@ -15,7 +20,7 @@ export class App {
   private loadingStore = inject(LoadingStore);
   private occupationService = inject(OccupationService);
 
-  isLoading = computed(() => 
-    this.loadingStore.isLoading() || this.occupationService.isLoading()
+  isLoading = computed(
+    () => this.loadingStore.isLoading() || this.occupationService.isLoading()
   );
 }
